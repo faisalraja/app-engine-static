5dc3ae8a9e832e6bce7135a22dcc49bc156b7c2a
Faisal Raja <faisalparveez@gmail.com>
2013-08-27 09:11:50 -0600

    added sitemap generator

--- generate.py
+++ generate.py
@@ -11,6 +11,8 @@ __author__ = 'faisal'
 hide_html_ext = False
 # Shows index.html on index paths
 use_index_paths = False
+# Enable/Disable sitemap generation / Enable by adding your live url
+sitemap = None  # 'http://www.example.com'
 
 url = 'http://localhost:8080/dev'
 template_path = 'templates'
@@ -28,6 +30,7 @@ def generate(path):
         return
 
     output = os.path.join(output_path, path)
+    url_path = path
 
     if hide_html_ext and not output.endswith('index.html'):
         output = output.split(os.sep)
@@ -36,6 +39,8 @@ def generate(path):
         output.append('.'.join(file))
         output.append('index.%s' % ext)
         output = os.sep.join(output)
+        # todo could this be variabled?
+        url_path = url_path.replace('.html', '/')
 
     output_dir = os.path.dirname(output)
 
@@ -60,8 +65,38 @@ def generate(path):
             f.write(response)
             print 'Generated %s' % output
 
+    return url_path
+
+
+sitemap_xml = '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+sitemap_format = """
+    <url>
+      <loc>{0}</loc>
+      <priority>0.5</priority>
+    </url>
+"""
 
 for r, d, f in os.walk(template_path):
     for files in f:
         if files.endswith('html'):
-            generate(os.path.join(r, files).replace(template_path + os.sep, ''))
+            p = generate(os.path.join(r, files).replace(template_path + os.sep, ''))
+
+            if sitemap and p:
+                sitemap_xml += sitemap_format.format('{0}/{1}'.format(sitemap, p))
+
+# write sitemap if modified
+if sitemap:
+    # close it
+    sitemap_xml += '</urlset>'
+
+    sitemap_path = os.path.join(output_path, 'sitemap.xml')
+    current_hash = None
+
+    if os.path.exists(sitemap_path):
+        with open(sitemap_path, 'r') as f:
+            current_hash = hashlib.sha1(f.read()).hexdigest()
+
+    if not current_hash or current_hash and current_hash != hashlib.sha1(sitemap_xml).hexdigest():
+        with open(sitemap_path, 'w') as f:
+            f.write(sitemap_xml)
+            print 'Generated %s' % sitemap_path
